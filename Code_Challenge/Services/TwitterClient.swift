986f701d30ec9bc4a918be7a2351b1b72d27f24f
Lotanna Igwe-Odunze <odunzelie@gmail.com>
2019-10-29 19:35:04 -0400

    added challenge folder

---
+++ Code_Challenge/Services/TwitterClient.swift
@@ -0,0 +1,103 @@
+//
+//  TwitterClient.swift
+//  Code Challenge
+//
+//  Created by Austin Feight on 11/21/18.
+//  Copyright © 2018 JOOR. All rights reserved.
+//
+
+import ReactiveKit
+
+protocol TwitterClientType {
+  var isLoggedIn: Property<Bool> { get }
+  
+  func logIn(username: String, password: String)
+  func logOut()
+  
+  func loadTimeline() -> [Tweet]
+}
+
+final class TwitterClient: TwitterClientType {
+  static let shared = TwitterClient()
+  
+  let isLoggedIn: Property<Bool>
+  
+  private static let isLoggedInKey = "isLoggedIn"
+  private let bag = DisposeBag()
+  
+  private init()
+  {
+    isLoggedIn = Property(UserDefaults.standard.bool(forKey: TwitterClient.isLoggedInKey))
+    isLoggedIn.observeNext { UserDefaults.standard.set($0, forKey: TwitterClient.isLoggedInKey) }
+              .dispose(in: bag)
+  }
+}
+
+// MARK: - Interface
+extension TwitterClient {
+  /**
+   Attempts to log in the user.
+   
+   On success, the property `isLoggedIn.value` will be set to `true`
+   
+   On failure, nothing will happen
+   
+   - Parameter username: The username to log in with. Valid username is `"user"`
+   - Parameter password: The password to log in with. Valid password is `"password"`
+  */
+  func logIn(username: String, password: String)
+  {
+    if users[username] == password {
+      isLoggedIn.value = true
+    }
+  }
+  
+  /// Sets the `isLoggedIn.value` property to `false`
+  func logOut()
+  {
+    isLoggedIn.value = false
+  }
+  
+  /// Returns a list of tweets
+  func loadTimeline() -> [Tweet]
+  {
+    return tweets
+  }
+}
+
+private let users = ["user": "password"]
+
+private let tweets = [
+  Tweet(id: 1, content: "Oh brother...", user: "Isaac Casanova"),
+  Tweet(id: 2, content: "No idea what this code is doing but LGTM 👍", user: "Austin Feight"),
+  Tweet(id: 3, content: "You miss 100% of the shots you don't miss - Wayne Gretsky - Michael Scott", user: "Jordan Buzzell"),
+  Tweet(id: 4, content: "I'm definitely not driving right now", user: "Dan Katz"),
+  Tweet(id: 5, content: "I did some jawns", user: "Ethan Riback"),
+  Tweet(id: 6, content: "It's short for Jebediah", user: "JBD"),
+  Tweet(id: 7, content: "I feel bad for my dog. I'll be dating a girl for months and then she's just gone and he has no idea why", user: "Bryan Pan"),
+  Tweet(id: 8, content: "1 S and 2 As god dammit", user: "Issac Casanova"),
+  Tweet(id: 9, content: "Something about aliens", user: "Allen Xu"),
+  Tweet(id: 10, content: "Send it", user: "Julia Fraenkel"),
+  Tweet(id: 11, content: "Let's nerd out and talk about stuff", user: "Derek Parham"),
+  Tweet(id: 12, content: "THIIIICCCCCCCC", user: "Jennifer Zhu"),
+  Tweet(id: 13, content: "Next season's biggest trend is see-through rain coats", user: "Kristin Savilia"),
+  Tweet(id: 14, content: "The team is alright", user: "Penny Xing"),
+  Tweet(id: 15, content: "Upstream... Downstream...", user: "Chad Cohen"),
+  Tweet(id: 16, content: "Imagine!", user: "Tenzing Dorjee"),
+  Tweet(id: 17, content: "Fake it til you make it", user: "Jacqueline Wu"),
+  Tweet(id: 18, content: "Not with that attitude", user: "Lawrence Li"),
+  Tweet(id: 19, content: "Hold on to your butts!", user: "Roberto Pedroso"),
+  Tweet(id: 20, content: "Call me PR papi", user: "James Cerrato"),
+  Tweet(id: 21, content: "*tries coffee* Whoa; is this what being alive feels like?", user: "Dan Katz"),
+  Tweet(id: 22, content: "The thing is: when you have a power drill, you win.", user: "Julia Fraenkel"),
+  Tweet(id: 23, content: "So if we stuck it in a lake, it would float. Because it's clearly a witch, cause that's sourcery.", user: "Ethan Riback"),
+  Tweet(id: 24, content: "You gotta turn your own wrenches, man.", user: "Kevin Maldjian"),
+  Tweet(id: 25, content: "I used to kick ass.", user: "Steve Lim"),
+  Tweet(id: 26, content: "I don't have a quote. I don't. It's not going to happen.", user: "Jackie Wu"),
+  Tweet(id: 27, content: "Life is juanderful", user: "Bryan Pan"),
+  Tweet(id: 28, content: "wHaT cOuLd gO wRoNg", user: "Katie McKew"),
+  Tweet(id: 29, content: "I think I'm addicted to emergen-c", user: "Katie McKew"),
+  Tweet(id: 30, content: "oui meat", user: "Bryan Pan"),
+  Tweet(id: 31, content: "This is the sort of hoodie I dreamed of when I was 19 (and going to career fairs)", user: "Juliet K. Benjamin"),
+  Tweet(id: 32, content: "also it hits different at 3am when you order it off the late night cravers menu with FREEDELV in the coupon box every time like a heathen", user: "Amanda Liu")
+]
---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '12.0'
+
+target 'Code_Challenge' do
+  use_frameworks!
+  inhibit_all_warnings!
+
+  pod 'Bond', '= 7.6.0'
+
+  target 'Code_ChallengeTests' do
+    inherit! :search_paths
+
+    pod 'Quick', '= 2.1.0'
+    pod 'Nimble', '= 8.0.1'
+  end
+end
