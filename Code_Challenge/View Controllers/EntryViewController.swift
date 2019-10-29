93286d88928c9a37347d81e923930407393c6450
Lotanna Igwe-Odunze <odunzelie@gmail.com>
2019-10-29 19:56:06 -0400

    created vc files

---
+++ Code_Challenge/View Controllers/EntryViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EntryViewController.swift
+//  Code_Challenge
+//
+//  Created by Lotanna Igwe-Odunze on 10/29/19.
+//  Copyright © 2019 JOOR. All rights reserved.
+//
+
+import Foundation
---
+++ Code_Challenge/View Controllers/TimelineViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TimelineViewController.swift
+//  Code_Challenge
+//
+//  Created by Lotanna Igwe-Odunze on 10/29/19.
+//  Copyright © 2019 JOOR. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UITableViewController {
+    
+    
+}
